3c381cfa56674caa2be353374d3b3d2ed64aab24
ssohy <sohyeonj415@gmail.com>
2022-08-17 22:23:39 +0900

    commit ssoh

--- app/src/main/java/com/example/project2/_4_T_s7Activity.java
+++ app/src/main/java/com/example/project2/_4_T_s7Activity.java
@@ -3,12 +3,76 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class _4_T_s7Activity extends AppCompatActivity {
+    TextView Seongnam,Sangok,BupyeongguOffice,Gulpocheon,SamsanGymnasium,Sangdong,BucheonCityHall,Sinjungdong,Chunui;
+    TextView BucheonStadium,Kkachiul,Onsu,Cheonwang,Gwangmyeongsageori,Cheolsan,GasanDigitalComplex,Namguro,Daerim,Sinpung;
+    TextView Boramae,Sindaebangsamgeori,Jangseungbaegi,Sangdo,SoongsilUniv,Namseong,ChongshinUniv,Naebang;
+    TextView ExpressBusTerminal,Banpo,Nonhyeon,Hakdong,GangnamguOffice,Cheongdam,TtukseomPark,KonkukUniv;
+    TextView ChildrensGrandPark,Gunja,Junggok,Yongmasan,Sagajeong,Myeonmok,Sangbong,Junghwa,Meokgol,Taereung;
+    TextView Gongneung,Hagye,Junggye,Nowon,Madeul,Suraksan,Dobongsan,Jangam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_ts7);
+
+        Myeonmok = findViewById(R.id.Myeonmok);
+        Sangbong = findViewById(R.id.Sangbong);
+        Junghwa = findViewById(R.id.Junghwa);
+        Meokgol = findViewById(R.id.Meokgol);
+        Taereung = findViewById(R.id.Taereung);
+        Gongneung = findViewById(R.id.Gongneung);
+        Hagye = findViewById(R.id.Hagye);
+        Junggye = findViewById(R.id.Junggye);
+        Nowon = findViewById(R.id.Nowon);
+        Madeul = findViewById(R.id.Madeul);
+        Suraksan = findViewById(R.id.Suraksan);
+        Dobongsan = findViewById(R.id.Dobongsan);
+        Jangam = findViewById(R.id.Jangam);
+
+        ExpressBusTerminal = findViewById(R.id.ExpressBusTerminal);
+        Banpo = findViewById(R.id.Banpo);
+        Hakdong = findViewById(R.id.Hakdong);
+        Nonhyeon = findViewById(R.id.Nonhyeon);
+        GangnamguOffice = findViewById(R.id.GangnamguOffice);
+        Cheongdam = findViewById(R.id.Cheongdam);
+        TtukseomPark = findViewById(R.id.TtukseomPark);
+        KonkukUniv = findViewById(R.id.KonkukUniv);
+        ChildrensGrandPark = findViewById(R.id.ChildrensGrandPark);
+        Gunja = findViewById(R.id.Gunja);
+        Junggok = findViewById(R.id.Junggok);
+        Yongmasan = findViewById(R.id.Yongmasan);
+        Sagajeong = findViewById(R.id.Sagajeong);
+
+        Gwangmyeongsageori = findViewById(R.id.Gwangmyeongsageori);
+        Cheolsan = findViewById(R.id.Cheolsan);
+        GasanDigitalComplex = findViewById(R.id.GasanDigitalComplex);
+        Namguro = findViewById(R.id.Namguro);
+        Daerim = findViewById(R.id.Daerim);
+        Boramae = findViewById(R.id.Boramae);
+        Sinpung = findViewById(R.id.Sinpung);
+        Sindaebangsamgeori = findViewById(R.id.Sindaebangsamgeori);
+        Jangseungbaegi = findViewById(R.id.Jangseungbaegi);
+        Sangdo = findViewById(R.id.Sangdo);
+        SoongsilUniv = findViewById(R.id.SoongsilUniv);
+        Namseong = findViewById(R.id.Namseong);
+        ChongshinUniv = findViewById(R.id.ChongshinUniv);
+        Naebang = findViewById(R.id.Naebang);
+
+        Seongnam = findViewById(R.id.Seongnam);
+        Sangok = findViewById(R.id.Sangok);
+        BupyeongguOffice = findViewById(R.id.BupyeongguOffice);
+        Gulpocheon = findViewById(R.id.Gulpocheon);
+        SamsanGymnasium = findViewById(R.id.SamsanGymnasium);
+        Sangdong = findViewById(R.id.Sangdong);
+        BucheonCityHall = findViewById(R.id.BucheonCityHall);
+        Sinjungdong = findViewById(R.id.Sinjungdong);
+        Chunui = findViewById(R.id.Chunui);
+        BucheonStadium = findViewById(R.id.BucheonStadium);
+        Kkachiul = findViewById(R.id.Kkachiul);
+        Onsu = findViewById(R.id.Onsu);
+        Cheonwang = findViewById(R.id.Cheonwang);
     }
 }
