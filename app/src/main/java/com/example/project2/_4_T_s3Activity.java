4465deaedcb369a4f47711f711db2f9fee8b57e4
ssohy <sohyeonj415@gmail.com>
2022-08-17 21:36:43 +0900

    commit ssoh

---
+++ app/src/main/java/com/example/project2/_4_T_s3Activity.java
@@ -0,0 +1,14 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class _4_T_s3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_4_ts3);
+    }
+}
