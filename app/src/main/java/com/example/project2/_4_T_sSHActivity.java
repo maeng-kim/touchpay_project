bd11a32be1ee65d4524418b582d8b7a7f2e712fb
ssohy <sohyeonj415@gmail.com>
2022-08-18 13:30:09 +0900

    commit ssoh

--- app/src/main/java/com/example/project2/_4_T_sSHActivity.java
+++ app/src/main/java/com/example/project2/_4_T_sSHActivity.java
@@ -3,12 +3,28 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class _4_T_sSHActivity extends AppCompatActivity {
+
+    TextView Sosa,Sosaeul,SiheungDaeya,Sincheon,Sinhyeon,SiheungCityHall,SiheungNeunggok,Dalmi,Seonbu,Choji,Siu,Wonsi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_ts_shactivity);
+
+        Sosa = findViewById(R.id.Sosa);
+        Sosaeul = findViewById(R.id.Sosaeul);
+        SiheungDaeya = findViewById(R.id.SiheungDaeya);
+        Sincheon = findViewById(R.id.Sincheon);
+        Sinhyeon = findViewById(R.id.Sinhyeon);
+        SiheungCityHall = findViewById(R.id.SiheungCityHall);
+        SiheungNeunggok = findViewById(R.id.SiheungNeunggok);
+        Dalmi = findViewById(R.id.Dalmi);
+        Seonbu = findViewById(R.id.Seonbu);
+        Choji = findViewById(R.id.Choji);
+        Siu = findViewById(R.id.Siu);
+        Wonsi = findViewById(R.id.Wonsi);
     }
 }
