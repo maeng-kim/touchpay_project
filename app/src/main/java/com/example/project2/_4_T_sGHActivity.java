00a6c149fd2d14b2c5be1d3565e51024232aedec
ssohy <sohyeonj415@gmail.com>
2022-08-18 13:18:42 +0900

    commit ssoh

--- app/src/main/java/com/example/project2/_4_T_sGHActivity.java
+++ app/src/main/java/com/example/project2/_4_T_sGHActivity.java
@@ -3,12 +3,30 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class _4_T_sGHActivity extends AppCompatActivity {
+
+    TextView SeoulStation, Gongdeok, HongikUniv, DigitalMediaCity, Magongnaru, GimpoIntlAirport, Gyeyang, Geomam, CheongnaIntlCity,Yeongjong, Unseo, IncheonIntlAirportCargoTerminal, IncheonIntlAirportTerminal1, IncheonIntlAirportTerminal2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_ts_ghactivity);
+
+        SeoulStation = findViewById(R.id.SeoulStation);
+        Gongdeok = findViewById(R.id.Gongdeok);
+        HongikUniv = findViewById(R.id.HongikUniv);
+        DigitalMediaCity = findViewById(R.id.DigitalMediaCity);
+        Magongnaru = findViewById(R.id.Magongnaru);
+        GimpoIntlAirport = findViewById(R.id.GimpoIntlAirport);
+        Gyeyang = findViewById(R.id.Gyeyang);
+        Geomam = findViewById(R.id.Geomam);
+        CheongnaIntlCity = findViewById(R.id.CheongnaIntlCity);
+        Yeongjong = findViewById(R.id.Yeongjong);
+        Unseo = findViewById(R.id.Unseo);
+        IncheonIntlAirportCargoTerminal = findViewById(R.id.IncheonIntlAirportCargoTerminal);
+        IncheonIntlAirportTerminal1 = findViewById(R.id.IncheonIntlAirportTerminal1);
+        IncheonIntlAirportTerminal2 = findViewById(R.id.IncheonIntlAirportTerminal2);
     }
 }
