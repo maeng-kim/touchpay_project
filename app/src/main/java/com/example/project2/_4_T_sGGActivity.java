39de482d774a40d321084d3e3a92609a00ae1a7f
ssohy <sohyeonj415@gmail.com>
2022-08-18 13:27:09 +0900

    commit ssoh

--- app/src/main/java/com/example/project2/_4_T_sGGActivity.java
+++ app/src/main/java/com/example/project2/_4_T_sGGActivity.java
@@ -3,12 +3,27 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class _4_T_sGGActivity extends AppCompatActivity {
+    TextView Pangyo, Imae, Samdong, GyeonggiGwangju, Chowol, Gonjiam, Sindundoyechon, Icheon, Bubal, Sejongdaewangneung, Yeoju ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_ts_ggactivity);
+
+        Pangyo = findViewById(R.id.Pangyo);
+        Imae = findViewById(R.id.Imae);
+        Samdong = findViewById(R.id.Samdong);
+        GyeonggiGwangju = findViewById(R.id.GyeonggiGwangju);
+        Chowol = findViewById(R.id.Chowol);
+        Gonjiam = findViewById(R.id.Gonjiam);
+        Sindundoyechon = findViewById(R.id.Sindundoyechon);
+        Pangyo = findViewById(R.id.Pangyo);
+        Icheon = findViewById(R.id.Icheon);
+        Bubal = findViewById(R.id.Bubal);
+        Sejongdaewangneung = findViewById(R.id.Sejongdaewangneung);
+        Yeoju = findViewById(R.id.Yeoju);
     }
 }
