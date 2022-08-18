2868eebe607d12ec77cab23adfe364f514c9facc
ssohy <sohyeonj415@gmail.com>
2022-08-18 13:14:17 +0900

    commit ssoh

--- app/src/main/java/com/example/project2/_4_T_sGCActivity.java
+++ app/src/main/java/com/example/project2/_4_T_sGCActivity.java
@@ -3,12 +3,41 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class _4_T_sGCActivity extends AppCompatActivity {
+    TextView Chuncheon, Namchuncheon, Gimyujeong, Gangchon, Baegyangri, Gulbongsan, Gapyeong, Sangcheon, Cheongpyeong, Daeseongri, Maseok, Cheonmasan, PyeongnaeHopyeong, Geumgok, Sareung, Toegyewon, Byeollae, Galmae, Sinnae, Mangu, Sangbong, KwangwoonUniv, Jungnang, Hoegi, Cheongnyangni ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_ts_gcactivity);
+
+        Chuncheon = findViewById(R.id.Chuncheon);
+        Namchuncheon = findViewById(R.id.Namchuncheon);
+        Gimyujeong = findViewById(R.id.Gimyujeong);
+        Gangchon = findViewById(R.id.Gangchon);
+        Baegyangri = findViewById(R.id.Baegyangri);
+        Gulbongsan = findViewById(R.id.Gulbongsan);
+        Gapyeong = findViewById(R.id.Gapyeong);
+        Sangcheon = findViewById(R.id.Sangcheon);
+        Cheongpyeong = findViewById(R.id.Cheongpyeong);
+        Daeseongri = findViewById(R.id.Daeseongri);
+        Maseok = findViewById(R.id.Maseok);
+        Cheonmasan = findViewById(R.id.Cheonmasan);
+        PyeongnaeHopyeong = findViewById(R.id.PyeongnaeHopyeong);
+        Geumgok = findViewById(R.id.Geumgok);
+        Sareung = findViewById(R.id.Sareung);
+        Toegyewon = findViewById(R.id.Toegyewon);
+        Byeollae = findViewById(R.id.Byeollae);
+        Galmae = findViewById(R.id.Galmae);
+        Sinnae = findViewById(R.id.Sinnae);
+        Mangu = findViewById(R.id.Mangu);
+        Sangbong = findViewById(R.id.Sangbong);
+        KwangwoonUniv = findViewById(R.id.KwangwoonUniv);
+        Jungnang = findViewById(R.id.Jungnang);
+        Hoegi = findViewById(R.id.Hoegi);
+        Cheongnyangni = findViewById(R.id.Cheongnyangni);
     }
 }
