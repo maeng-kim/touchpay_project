7d96ffa35a8a82b172d46fd2b7d1a3c4ab86a368
ssohy <sohyeonj415@gmail.com>
2022-08-18 13:20:51 +0900

    commit ssoh

--- app/src/main/java/com/example/project2/_4_T_sSRActivity.java
+++ app/src/main/java/com/example/project2/_4_T_sSRActivity.java
@@ -3,12 +3,27 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class _4_T_sSRActivity extends AppCompatActivity {
+    TextView Saetgang,Daebang,SeoulRegionalOfficeofMilitaryManpower,Boramae,BoramaePark,BoramaeMedicalCenter,Danggok,Sillim,Seowon,SeoulNatlUnivVentureTown,Gwanaksan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_ts_sractivity);
+
+        Saetgang = findViewById(R.id.Saetgang);
+        Daebang = findViewById(R.id.Daebang);
+        SeoulRegionalOfficeofMilitaryManpower = findViewById(R.id.SeoulRegionalOfficeofMilitaryManpower);
+        Boramae = findViewById(R.id.Boramae);
+        BoramaePark = findViewById(R.id.BoramaePark);
+        BoramaeMedicalCenter = findViewById(R.id.BoramaeMedicalCenter);
+        Danggok = findViewById(R.id.Danggok);
+        Sillim = findViewById(R.id.Sillim);
+        Seowon = findViewById(R.id.Seowon);
+        SeoulNatlUnivVentureTown = findViewById(R.id.SeoulNatlUnivVentureTown);
+        Gwanaksan = findViewById(R.id.Gwanaksan);
+
     }
 }
